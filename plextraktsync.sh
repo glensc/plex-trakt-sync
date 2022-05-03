eda6a1c28ff627c9977693160ad6c33b84b7a6d0
Elan Ruusamäe <glen@pld-linux.org>
2022-05-03 14:55:01 +0300

    Without arguments, run sync first, then continue with watch

--- plextraktsync.sh
+++ plextraktsync.sh
@@ -5,4 +5,11 @@ file=$(readlink -f "$0")
 dir=$(dirname "$file")
 
 cd "$dir"
+
+# without arguments, run sync first, then continue with watch
+if [ -z "${1:-}" ]; then
+	pipenv run plextraktsync sync
+	set -- watch
+fi
+
 exec pipenv run plextraktsync "$@"
